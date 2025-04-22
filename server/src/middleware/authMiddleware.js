bbc2795830831fe4e881d2693b766c3dca57bd70
Veer <balvirsd2000@gmail.com>
2025-04-22 18:14:06 +0530

    auth error fix

--- server/src/middleware/authMiddleware.js
+++ server/src/middleware/authMiddleware.js
@@ -2,15 +2,10 @@ import jwt from "jsonwebtoken";
 
 const authUser = async (req, res, next) => {
   const { token } = req.cookies;
-
-  if (!token) {
-    return res.status(401).json({ success: false, message: 'Not Authorized' });
-  }
-
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     if (decoded.id) {
-      req.userId = decoded.id; // ✅ safer than modifying req.body
+      req.userId = decoded.id; 
       next();
     } else {
       return res.status(401).json({ success: false, message: 'Not Authorized' });
